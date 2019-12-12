3463d56056c4316e0888904a1bc1d931a6c92399
ping40 <norberthu30@gmail.com>
2019-12-12 21:10:13 +0800

    list

--- apps/pis/src/app/ebbinghaus/ebbinghaus.module.ts
+++ apps/pis/src/app/ebbinghaus/ebbinghaus.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { NewComponent } from './new/new.component';
+import { CommonMaterialModule } from '../common/common.material';
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent, DetailComponent, NewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CommonMaterialModule
   ]
 })
 export class EbbinghausModule { }
